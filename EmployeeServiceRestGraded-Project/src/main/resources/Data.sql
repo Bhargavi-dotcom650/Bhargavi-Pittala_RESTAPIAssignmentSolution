c3afdf1b7cc40584502bbbf0449379106a7429a9
Bhargavi-dotcom650 <bpittala490@gmail.com>
2022-09-10 15:49:20 +0530

    Initial Commit

---
+++ EmployeeServiceRestGraded-Project/src/main/resources/Data.sql
@@ -0,0 +1,5 @@
+insert into roles (role_id,name) values (1,'ADMIN');
+
+insert into users (user_id,password,username) values (1,'$2a$12$mCr9zE8Mc3C7K2stVCMKYeXfLLvIsO4aPHo9PSDW3T9T20eRgDBN2','bhargavi');
+
+insert into users_roles (user_id,role_id) values (1,1);
